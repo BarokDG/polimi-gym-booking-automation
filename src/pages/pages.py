e80293404be0e1082d4700d0fe1da74c3729b089
barokdg <dagimbarok@gmail.com>
2026-09-06 18:46:12 +0200

    ci: set environment for "run" job

--- src/pages/pages.py
+++ src/pages/pages.py
@@ -17,10 +17,6 @@ USERNAME = os.environ["USERNAME"]
 PASSWORD = os.environ["PASSWORD"]
 TOKEN = os.environ["TOKEN"]
 
-if not USERNAME or not PASSWORD or not TOKEN:
-    print(USERNAME, PASSWORD, TOKEN)
-    raise ValueError("USERNAME, PASSWORD, and TOKEN environment variables must be set.")
-
 
 class Page(ABC):
     def __init__(self, driver: WebDriver):
